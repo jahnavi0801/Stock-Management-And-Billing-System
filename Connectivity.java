fae9285d5ff797e029b9887edef22005810a34e9
jahnavi0801 <53981448+jahnavi0801@users.noreply.github.com>
2019-10-31 19:25:29 +0530

    check

--- Connectivity.java
+++ Connectivity.java
@@ -8,7 +8,7 @@ public interface Connectivity {
     default Statement Connect() throws Exception{
     	try {
     		Class.forName("com.mysql.jdbc.Driver");
-    		Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/product","root","Josaa_16651");
+    		Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/product?useSSL=false","root","Josaa_16651");
     	    Statement s = c.createStatement();
     		return s;
     	}catch(Exception e) {
